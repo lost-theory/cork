df14d2f616027218ffb4d69ef95c0df3664a0995
Alex Morega <alex@grep.ro>
2009-03-16 23:14:09 +0200

    repository support and note inheritance

--- cork/__init__.py
+++ cork/__init__.py
@@ -1,7 +1,8 @@
 import yaml
 
 class CorkNote(dict):
-    def __init__(self, content):
+    def __init__(self, content, repo=None):
+        self.repo = repo
         self.update(content)
         def walk(obj):
             if isinstance(obj, CorkMethod):
@@ -17,20 +18,75 @@ class CorkNote(dict):
     def __repr__(self):
         return '<CorkNote at 0x%x>' % id(self)
 
+    def __getitem__(self, key):
+        if key in self:
+            val = dict.get(self, key)
+
+        elif key != '__inherit__' and '__inherit__' in self:
+            superclass = self['__inherit__']
+            if superclass not in self.repo:
+                raise NotImplementedError # TODO
+
+            val = self.repo[superclass][key]
+
+        else:
+            raise KeyError('CorkNote has no member named "%s"' % key)
+
+        if isinstance(val, CorkMethod):
+            val = val.bind(self)
+        return val
+
+
 def note_constructor(loader, node):
     value = loader.construct_mapping(node)
     return CorkNote(value)
 yaml.add_constructor(u'!note', note_constructor)
 
 class CorkMethod(object):
-    def __init__(self, code):
+    def __init__(self, code, note=None):
         self.code = code
+        self.note = note
 
     def __repr__(self):
         return '<CorkMethod at 0x%x>' % id(self)
 
     def __call__(self, *args, **kwargs):
         return self.code['__call__'](self.note, *args, **kwargs)
+
+    def bind(self, note):
+        return CorkMethod(self.code, note)
+
+class CorkRepo(object):
+    def __init__(self, repo_dir):
+        self.repo_dir = repo_dir
+
+    def __repr__(self):
+        return '<CorkRepo at "%s">' % self.repo_dir.encode('ascii', 'replace')
+
+    def _get_note_list(self):
+        import os
+        notes = set()
+        #for dirpath, dirnames, filenames in os.walk(self.repo_dir):
+        #    dirpath = dirpath[len(self.repo_dir):]
+        #    notes.update(dirpath + filename for filename in filenames)
+        for filename in os.listdir(self.repo_dir):
+            if filename.endswith('.note'):
+                notes.add('/%s' % filename[:-5])
+        return notes
+
+    def list_notes(self):
+        return self._get_note_list()
+
+    def __len__(self):
+        return len(self._get_note_list())
+
+    def __getitem__(self, key):
+        # TODO: unsafe!
+        raw_data = open('%s%s.note' % (self.repo_dir, key), 'rb').read()
+        return CorkNote(yaml.load(raw_data), repo=self)
+
+    def __contains__(self, key):
+        return key in self._get_note_list()
 
 def method_constructor(loader, node):
     code = loader.construct_scalar(node)
@@ -43,3 +99,6 @@ yaml.add_constructor(u'!py', method_constructor)
 
 def parse_note(raw_data):
     return CorkNote(yaml.load(raw_data))
+
+def open_repo(repo_dir):
+    return CorkRepo(repo_dir)
--- tests/doctests.py
+++ tests/doctests.py
@@ -3,10 +3,11 @@ import doctest
 from os.path import normpath, dirname, join
 
 suite = unittest.TestSuite()
-doc_file = normpath(join(dirname(__file__), '../docs/note_format.txt'))
-text = open(doc_file, 'rb').read().decode('utf-8')
-doc_test = doctest.DocTestParser().get_doctest(text, {}, 'Note format', 'note_format.txt', 0)
-suite.addTest(doctest.DocTestCase(doc_test, optionflags=doctest.ELLIPSIS))
+for fname in ['note_format.txt', 'repository.txt']:
+    doc_file = normpath(join(dirname(__file__), '../docs/%s' % fname))
+    text = open(doc_file, 'rb').read().decode('utf-8')
+    doc_test = doctest.DocTestParser().get_doctest(text, {}, fname, fname, 0)
+    suite.addTest(doctest.DocTestCase(doc_test, optionflags=doctest.ELLIPSIS))
 
 if __name__ == '__main__':
     runner = unittest.TextTestRunner()
